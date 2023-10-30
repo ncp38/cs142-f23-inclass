6bcdee0cb95ad379b116cdf3a02773675a982f13
Nate Phillips <anemailanemailmykingdom@gmail.com>
2023-10-29 22:31:13 -0500

    Update Recursion3.java

--- src/recursion/Recursion3.java
+++ src/recursion/Recursion3.java
@@ -7,11 +7,11 @@ public class Recursion3 {
     public static void main(String[] args)
     {
         // test sum of arraylist:
-        ArrayList<Integer> mylist = new ArrayList<Integer>(Arrays.asList(2, 4, 6));
-        System.out.println("Sum is " + sumOfArrayList(mylist));
+        //ArrayList<Integer> mylist = new ArrayList<Integer>(Arrays.asList(2, 4, 6));
+        //System.out.println("Sum is " + sumOfArrayList(mylist));
 
         // test max of arraylist:
-
+        System.out.println(maxOfArray( new int[] {2, 9, 11, 43, -963, 22, -1}));
     }
 
     // Calculate the sum of all the elements in an arraylist.
@@ -33,15 +33,28 @@ public class Recursion3 {
     }
 
     // Calculate the largest element in an arraylist.
-    public static int maxOfArrayList(ArrayList<Integer> list)
+    public static int maxOfArray(int[] array)
     {
-        return 0;
+        return maxOfArray(array, 0);
     }
 
-    // Helper function for above.
-    private static int maxOfArrayList(ArrayList<Integer> list, int leftIdx)
+    // Helper function for above.  Assumes an array with one or more ints.
+    private static int maxOfArray(int[] array, int leftIdx)
     {
-        return 0;
+        if (leftIdx == array.length-1) {  // if there's only one element in our "sub-list"
+            return array[leftIdx];
+        }
+        else {
+            int recursiveMax = maxOfArray(array, leftIdx + 1);
+            if(array[leftIdx] >= recursiveMax)
+            {
+                return array[leftIdx];
+            }
+            else
+            {
+                return recursiveMax;
+            }
+        }
     }
 
 }
